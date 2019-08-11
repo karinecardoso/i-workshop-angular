6674ffc495b43cda8eb54c4dddb747f3163ab10f
Karine Cardoso <karine.cardoso_@hotmail.com>
2019-08-11 20:20:58 -0300

    Cria componente de lista de contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule
