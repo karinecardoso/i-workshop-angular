5ffe0ede0cd4b7bdde7ae77f3ea1088091a35c8f
Karine Cardoso <karine.cardoso_@hotmail.com>
2019-08-11 23:55:14 -0300

    Integra componente contact-list com a service

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+  contacts;
 
-  constructor() { }
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
+    this.contactService.getContacts()
+      .subscribe(contacts => this.contacts = contacts);
   }
 
 }
